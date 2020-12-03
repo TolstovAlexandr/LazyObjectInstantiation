965fb354aaf632a6bd9e7b6ffdf2864522b8a558
Alexandr Tolstov <SESA574650@se.com>
2020-12-03 09:15:21 +0600

    Add project files.

---
+++ LazyObjectInstantiation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectInstantiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lazy Instantiation *****\n");
+            // This caller does not care about getting all songs,
+            // but indirectly created 10,000 objects!
+            MediaPlayer myPlayer = new MediaPlayer();
+            myPlayer.Play();
+
+            // Allocation of AllTracks happens when you call GetAllTracks().
+            MediaPlayer yourPlayer = new MediaPlayer();
+            AllTracks yourMusic = yourPlayer.GetAllTracks();
+        }
+    }
+}
